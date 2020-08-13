eb8b26982c585756e586def50d483bc82d91c6ea
Dipanshu Singh <69574915+Dipanshu8@users.noreply.github.com>
2020-08-13 13:55:24 +0530

    Update Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -1,176 +1,12 @@
-# Student-Data-Management
 //Include all the necessary libraries. 
 #include<stdio.h> 
 #include<iostream> 
 #include<fstream> 
 #include<string.h> 
 
-	using namespace std; 
+using namespace std; 
 
-	int main(){ 
-	char ch
-	cout << "\n \t\t\t 1. Basic Features";
-        cout << "\n \t\t\t 2. Advance Features";
-        cout << "\t\t\t Select Your Choice :=> ";
-        fflush(stdin);
-        ch = getche();	
-	case '1':
-	//This code is for basic Features
-		FILE *fp, *ft;
-    	char another, choice;
-
-    	struct student
-    	{
-        char first_name[50], last_name[50];
-        char course[100];
-        int section;
-   	 };
-
-   	 struct student e;
-    	char xfirst_name[50], xlast_name[50];
-    	long int recsize;
-
-    	fp=fopen("users.txt","rb+");
-
-    	if (fp == NULL)
-    	{
-        	fp = fopen("users.txt","wb+");
-
-        	if (fp==NULL)
-        	{
-            	puts("Cannot open file");
-            	return 0;
-        	}
-    	}
-
-
-   	 recsize = sizeof(e);
-
-    	while(1)
-   	 {
-        	system("cls");
-
-        	cout << "\t\tSTUDENT DATABASE MANAGEMENT SYSTEM ";
-        	cout <<"\n\n                                          ";
-        	cout << "\n\n";
-        	cout << "\n \t\t\t 1. Add    Records";
-        	cout << "\n \t\t\t 2. List   Records";
-       		cout << "\n \t\t\t 3. Modify Records";
-       		cout << "\n \t\t\t 4. Delete Records";
-        	cout << "\n \t\t\t 5. Exit   Program";
-        	cout << "\n\n";
-        	cout << "\t\t\t Select Your Choice :=> ";
-        	fflush(stdin);
-        	choice = getche();
-        	switch(choice)
-        	{
-        	case '1' :
-		//This is to add student records		
-            	fseek(fp,0,SEEK_END);
-            	another ='Y';
-            	while(another == 'Y' || another == 'y')
-            	{
-                	system("cls");
-                	cout << "Enter the Firt Name : ";
-                	cin >> e.first_name;
-                	cout << "Enter the Last Name : ";
-                	cin >> e.last_name;
-                	cout << "Enter the Course    : ";
-                	cin >> e.course;
-                	cout << "Enter the Section   : ";
-                	cin >> e.section;
-                	fwrite(&e,recsize,1,fp);
-                	cout << "\n Add Another Record (Y/N) ";
-                	fflush(stdin);
-                	another = getchar();
-            	}
-            	break;
-        	case '2':
-            	system("cls");
-            	rewind(fp);
-            	cout << "=== View the Records in the Database ===";
-            	cout << "\n";
-            	while (fread(&e,recsize,1,fp) == 1)
-            	{
-                	cout << "\n";
-                	cout <<"\n" << e.first_name << setw(10)  << e.last_name;
-                	cout << "\n";
-                	cout <<"\n" <<e.course <<  setw(8)  << e.section;
-            	}
-            	cout << "\n\n";
-            	system("pause");
-            	break;
-
-        	case '3' :
-            	system("cls");
-            	another = 'Y';
-            	while (another == 'Y'|| another == 'y')
-            	{
-                	cout << "\n Enter the last name of the student : ";
-                	cin >> xlast_name;
-
-                	rewind(fp);
-                	while (fread(&e,recsize,1,fp) == 1)
-                	{
-                    	if (strcmp(e.last_name,xlast_name) == 0)
-                    	{
-                        	cout << "Enter new the Firt Name : ";
-                        	cin >> e.first_name;
-                        	cout << "Enter new the Last Name : ";
-                        	cin >> e.last_name;
-                        	cout << "Enter new the Course    : ";
-                        	cin >> e.course;
-                        	cout << "Enter new the Section   : ";
-                        	cin >> e.section;
-                        	fseek(fp, - recsize, SEEK_CUR);
-                        	fwrite(&e,recsize,1,fp);
-                        	break;
-                    	}
-                    	else
-                        	cout<<"record not found";
-                	}
-                	cout << "\n Modify Another Record (Y/N) ";
-                	fflush(stdin);
-                	another = getchar();
-            	}
-            	break;
-
-
-        	case '4':
-            	system("cls");
-            	another = 'Y';
-            	while (another == 'Y'|| another == 'y')
-            	{
-                	cout << "\n Enter the last name of the student to delete : ";
-                	cin >> xlast_name;
-
-                	ft = fopen("temp.dat", "wb");
-
-                	rewind(fp);
-                	while (fread (&e, recsize,1,fp) == 1)
-
-                    	if (strcmp(e.last_name,xlast_name) != 0)
-                    	{
-                        	fwrite(&e,recsize,1,ft);
-                    	}
-                	fclose(fp);
-                	fclose(ft);
-                	remove("users.txt");
-                	rename("temp.dat","users.txt");
-
-                	fp=fopen("users.txt","rb+");
-
-                	cout << "\n Delete Another Record (Y/N) ";
-                	fflush(stdin);
-                	another = getchar();
-            	}
-
-            	break;
-
-       
-    	}
-	//This code is for advance features
-		case '2':		
+int main(){ 
 	//Considering the max length of data entered (name) to be 15. 
 	char data[15]; 
 	int n = 0, option = 0, count_n = 0; 
@@ -186,7 +22,7 @@
 		count_n++;} 
 	
 	while(option != 6){ 
-	//This prints out all the available options in the DB 
+//This prints out all the available options in the DB 
 	cout << "\nAvailable operations: \n1. Add New Students\n2."
 		<< "Student Login\n3. Faculty Login\n4. Proctor Login\n5. Admin View\n"
 		<< "6. Exit\nEnter option: "; 
@@ -401,6 +237,4 @@
 			} 
 		} 
 	} 
-	break;
 }} 
-
